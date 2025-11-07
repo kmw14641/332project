1808bd3ca63d55e01bbf9584736cb48c38001129
Nemo <ejongwon7@gmail.com>
2025-11-07 16:35:30 +0000

    Feat: implements MasterService.registerWorker
    - it registers workers and print when it is done

---
+++ master/src/main/scala/master/MasterServiceImpl.scala
@@ -0,0 +1,27 @@
+package master
+
+import io.grpc.{Server, ServerBuilder}
+import scala.concurrent.{ExecutionContext, Future}
+import master.MasterService.{MasterServiceGrpc, WorkerInfo, RegisterWorkerResponse}
+import master.Master
+
+// TODO: handling fault tolerance
+// 1. worker shutdown after Master.registerWorker
+// 2. worker shutdown after return the registerWorker
+// => then in sampling phase, 1 or 2 must be delayed so
+// 1. worker gives registerWorker again => just update
+// 2. worker gives samples => register worker works well, dont care about it
+class MasterServiceImpl(implicit ec: ExecutionContext) extends MasterServiceGrpc.MasterService {
+  override def registerWorker(request: WorkerInfo): Future[RegisterWorkerResponse] = {
+    val workerIp = request.ip
+
+    Master.registerWorker(request)
+    if (Master.getRegisteredWorkers.size == Master.getWorkersNum) {
+      println(Master.getRegisteredWorkers.keys.mkString(", "))
+    }
+
+    Future.successful(
+      RegisterWorkerResponse(success = true)
+    )
+  }
+}
