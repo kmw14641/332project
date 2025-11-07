c0a6b035a3116e2cbc5ff7e3f29bbbc687fb1323
Nemo <ejongwon7@gmail.com>
2025-11-07 14:03:15 +0000

    Feat: splits ip and port from option value

--- worker/src/main/scala/Main.scala
+++ worker/src/main/scala/Main.scala
@@ -4,4 +4,9 @@ object Main extends App {
   val (masterAddr, inputDirs, outputDir) = WorkerOptionUtils.parse(args).getOrElse {
     sys.exit(1)
   }
+  
+  val (masterIp, masterPort) = {
+    val parts = masterAddr.split(":")
+    (parts(0), parts(1).toInt)
+  }
 }
