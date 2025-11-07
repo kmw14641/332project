a1d62192fae70e06ac3956d992444c3cdf1f3e83
Nemo <ejongwon7@gmail.com>
2025-11-07 16:38:54 +0000

    Feat: registers self to master when server opened
    - server is naive no-functional server just for getport code

--- worker/src/main/scala/Main.scala
+++ worker/src/main/scala/Main.scala
@@ -1,6 +1,12 @@
+import io.grpc.ServerBuilder
+import scala.concurrent.ExecutionContext
+import utils.{WorkerOptionUtils, PathUtils, SystemUtils}
+import master.MasterClient
 import worker.Worker
 
 object Main extends App {
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
   val (masterAddr, inputDirs, outputDir) = WorkerOptionUtils.parse(args).getOrElse {
     sys.exit(1)
   }
@@ -23,4 +29,22 @@ object Main extends App {
   Worker.setMasterAddr(masterIp, masterPort)
   Worker.setInputDirs(inputDirs)
   Worker.setOutputDir(outputDir)
+
+  val workerIp = SystemUtils.getLocalIp.getOrElse {
+    println("Failed to get local IP address")
+    sys.exit(1)
+  }
+  
+  val ramMb = SystemUtils.getRamMb
+
+  val server = ServerBuilder
+    .forPort(0)
+    .build()
+
+  server.start()
+
+  val port = server.getPort
+
+  val client = new MasterClient(masterIp, masterPort)
+  client.registerWorker(workerIp, port, ramMb)
 }
