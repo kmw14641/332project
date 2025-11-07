e5248a0ca308e4e93dc2c2d17391c0f34ad754b7
Nemo <ejongwon7@gmail.com>
2025-11-07 13:33:39 +0000

    Feat: implements utils to handle worker option
    - <master ip:port>
    - -I <input_dir> ... => multiple directories are possible
    - -O <output_dir>

---
+++ worker/src/main/scala/utils/WorkerOptionUtils.scala
@@ -0,0 +1,54 @@
+package utils
+
+import scopt.OParser
+
+case class WorkerConfig(
+  masterAddr: String = "",
+  inputDirectories: Seq[String] = Seq.empty,
+  outputDirectory: String = ""
+)
+
+object WorkerOptionUtils {
+  private def getParser: OParser[Unit, WorkerConfig] = {
+    val builder = OParser.builder[WorkerConfig]
+    val parser = {
+      import builder._
+      OParser.sequence(
+        programName("worker"),
+        head("worker", "1.0"),
+        arg[String]("<master ip:port>")
+          .required()
+          .action((x, c) => c.copy(masterAddr = x))
+          .text("master address in format ip:port")
+          .validate(x =>
+            if (x.contains(":")) success
+            else failure("master address must be in format ip:port")
+          ),
+        opt[Seq[String]]('I', "input")
+          .required()
+          .unbounded()
+          .valueName("<dir1> <dir2>...")
+          .action((x, c) => c.copy(inputDirectories = c.inputDirectories ++ x))
+          .text("input directories")
+          .validate(x =>
+            if (x.nonEmpty) success
+            else failure("at least one input directory must be specified")
+          ),
+        opt[String]('O', "output")
+          .required()
+          .action((x, c) => c.copy(outputDirectory = x))
+          .text("output directory")
+          .validate(x =>
+            if (x.nonEmpty) success
+            else failure("output directory must not be empty")
+          )
+      )
+    }
+
+    parser
+  }
+
+  def parse(args: Array[String]): Option[(String, Seq[String], String)] = OParser.parse(getParser, args, WorkerConfig()).map {
+    config => (config.masterAddr, config.inputDirectories, config.outputDirectory)
+  }
+}
