a25ac27408aef96beea6ca515936e34627d95df6
Nemo <ejongwon7@gmail.com>
2025-11-07 14:21:31 +0000

    feat: checks the validity of directories
    - checks all input dirs exist, otherwise program terminates
    - creates output dir if it does not exist

--- worker/src/main/scala/Main.scala
+++ worker/src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import utils.WorkerOptionUtils
+import utils.{WorkerOptionUtils, PathUtils}
 
 object Main extends App {
   val (masterAddr, inputDirs, outputDir) = WorkerOptionUtils.parse(args).getOrElse {
@@ -9,4 +9,15 @@ object Main extends App {
     val parts = masterAddr.split(":")
     (parts(0), parts(1).toInt)
   }
+
+  val invalidInputDirs = inputDirs.filter{ dir => !PathUtils.exists(dir) || !PathUtils.isDirectory(dir) }
+
+  if (invalidInputDirs.nonEmpty) {
+    invalidInputDirs.foreach { dir =>
+      Console.err.println(s"Input directory does not exist or is not a directory: $dir(${PathUtils.toAbsolutePath(dir)})")
+    }
+    sys.exit(1)
+  }
+
+  PathUtils.createDirectoryIfNotExists(outputDir)
 }
