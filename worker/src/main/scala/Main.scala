8d01aa64795cf4be66e4e80bd910388d7aeccd02
Nemo <ejongwon7@gmail.com>
2025-11-07 14:21:31 +0000

    Feat: checks directories all exist

--- worker/src/main/scala/Main.scala
+++ worker/src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import utils.WorkerOptionUtils
+import utils.{WorkerOptionUtils, PathUtils}
 
 object Main extends App {
   val (masterAddr, inputDirs, outputDir) = WorkerOptionUtils.parse(args).getOrElse {
@@ -9,4 +9,14 @@ object Main extends App {
     val parts = masterAddr.split(":")
     (parts(0), parts(1).toInt)
   }
+
+  inputDirs.foreach {
+    dir =>
+      if (!PathUtils.exists(dir) || !PathUtils.isDirectory(dir)) {
+        println(s"Input directory does not exist or is not a directory: $dir(${PathUtils.toAbsolutePath(dir)})")
+        sys.exit(1)
+      }
+  }
+
+  PathUtils.createDirectoryIfNotExists(outputDir)
 }
