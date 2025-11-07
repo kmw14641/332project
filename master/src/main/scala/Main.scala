1074a79621df098daeeecaa0bc0f2cd4fe5891d5
Nemo <ejongwon7@gmail.com>
2025-11-07 12:52:53 +0000

    Feat: adds naive server initialize logic

--- master/src/main/scala/Main.scala
+++ master/src/main/scala/Main.scala
@@ -13,5 +13,24 @@ object Main extends App {
     case None => sys.exit(1)
   }
 
-  println(s"Starting master with $workersNum workers")
+  val ip = utils.AddrUtils.getAddr match {
+    case Some(addr) => addr
+    case None =>
+      println("Failed to get local IP address")
+      sys.exit(1)
+  }
+
+  val server = ServerBuilder
+  .forPort(0)
+  .addService(GreeterGrpc.bindService(new GreeterImpl(), ec))
+  .build()
+
+  server.start()
+
+  val port = server.getPort
+  println(s"$ip:$port")
+
+  new GreeterClient().fansign()
+
+  server.awaitTermination()
 }
