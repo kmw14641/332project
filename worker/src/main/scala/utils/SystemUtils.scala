8422fca18af4f7746690d64d3ec65ccc34951905
Nemo <ejongwon7@gmail.com>
2025-11-07 16:34:30 +0000

    Feat: implements utils for getting localIP and ram_mb

---
+++ worker/src/main/scala/utils/SystemUtils.scala
@@ -0,0 +1,13 @@
+package utils
+
+import java.net.InetAddress
+
+object SystemUtils {
+  def getLocalIp: Option[String] = scala.util.Try(InetAddress.getLocalHost.getHostAddress).toOption
+
+  def getRamMb: Long = {
+    val runtime = Runtime.getRuntime
+    val maxMemory = runtime.maxMemory()
+    maxMemory / (1024 * 1024)
+  }
+}
