cebe89b754e980959f5b64ace327079ff50667b1
Nemo <ejongwon7@gmail.com>
2025-11-07 13:05:09 +0000

    Refactor: use Try instead of try catch

--- master/src/main/scala/utils/AddrUtils.scala
+++ master/src/main/scala/utils/AddrUtils.scala
@@ -1,15 +1,7 @@
 package utils
 
 import java.net.InetAddress
-import java.net.UnknownHostException
 
 object AddrUtils {
-  def getAddr: Option[String] = {
-    try {
-      val addr = InetAddress.getLocalHost.getHostAddress
-      Some(addr)
-    } catch {
-      case _: UnknownHostException => None
-    }
-  }
+  def getAddr: Option[String] = scala.util.Try(InetAddress.getLocalHost.getHostAddress).toOption
 }
