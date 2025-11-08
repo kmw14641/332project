8e2457aaf8c75a3cce904a440ab7fd641ab2996c
Nemo <ejongwon7@gmail.com>
2025-11-08 10:32:51 +0000

    feat: implements sampling rpc
    - gets samples
    - if all samples are gathered, sperad ranges to workers

--- master/src/main/scala/master/MasterServiceImpl.scala
+++ master/src/main/scala/master/MasterServiceImpl.scala
@@ -2,7 +2,7 @@ package master
 
 import io.grpc.{Server, ServerBuilder}
 import scala.concurrent.{ExecutionContext, Future}
-import master.MasterService.{MasterServiceGrpc, WorkerInfo, RegisterWorkerResponse}
+import master.MasterService.{MasterServiceGrpc, WorkerInfo, RegisterWorkerResponse, SampleData, SampleResponse}
 import master.Master
 
 // TODO: handling fault tolerance
@@ -21,5 +21,46 @@ class MasterServiceImpl(implicit ec: ExecutionContext) extends MasterServiceGrpc
     Future.successful(
       RegisterWorkerResponse(success = true)
     )
+  }
+
+  override def sampling(request: SampleData): Future[SampleResponse] = {
+    val keys = request.keys
+    val success = Master.addSamples(request.workerIp, keys)
+
+    // If ranges are ready, trigger range assignment
+    if (Master.isRangesReady) {
+      // Spawn a separate thread to assign ranges to workers
+      Future {
+        Thread.sleep(500)  // Small delay to ensure all responses are sent
+        assignRangesToWorkers()
+      }
+    }
+
+    Future.successful(
+      SampleResponse(success = success)
+    )
+  }
+
+  private def assignRangesToWorkers(): Unit = {
+    val workers = Master.getRegisteredWorkers.toSeq.sortBy(_._1)  // Sort by IP for consistent ordering
+    val ranges = Master.getRanges
+
+    println("Assigning ranges to workers...")
+
+    // TODO: use multithreading to speed up
+    workers.zipWithIndex.foreach { case ((workerIp, workerInfo), idx) =>
+      if (idx < ranges.length) {
+        val (rangeStart, rangeEnd) = ranges(idx)
+        
+        // Create client to send range to worker
+        val workerClient = new worker.WorkerClient(workerInfo.ip, workerInfo.port)
+        try {
+          workerClient.sampled(rangeStart, rangeEnd)
+        } catch {
+          case e: Exception =>
+            println(s"Failed to assign range to worker $workerIp:${workerInfo.port}: ${e.getMessage}")
+        }
+      }
+    }
   }
 }
