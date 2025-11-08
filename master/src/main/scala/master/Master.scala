f4a39af7e2c154c78158aa1235309ed95051045a
Nemo <ejongwon7@gmail.com>
2025-11-08 06:09:24 +0000

    Fix: race condition can occur so print logic moves to Master(synced)

--- master/src/main/scala/master/Master.scala
+++ master/src/main/scala/master/Master.scala
@@ -14,10 +14,28 @@ object Master {
   def getWorkersNum: Int = this.synchronized {
     workersNum
   }
-  
-  def registerWorker(request: WorkerInfo): Unit = this.synchronized {
+
+  def registerWorker(request: WorkerInfo): Boolean = this.synchronized {
     val workerIp = request.ip
-    registeredWorkers += (workerIp -> request)
+    if (registeredWorkers.size == workersNum) {
+      if (!registeredWorkers.contains(workerIp)) false
+      else {
+        registeredWorkers += (workerIp -> request)
+        println(s"Fault detected! Re-register worker($workerIp:${request.port})")
+        println(registeredWorkers.keys.mkString(", "))
+        
+        true
+      }
+    }
+    else {
+      val previouslyFull = registeredWorkers.size == workersNum
+      registeredWorkers += (workerIp -> request)
+      if (!previouslyFull && registeredWorkers.size == workersNum) {
+        println(registeredWorkers.keys.mkString(", "))
+      }
+
+      true
+    }
   }
 
   def getRegisteredWorkers: Map[String, WorkerInfo] = this.synchronized {
