6541156738a022c6f27782537a847e874b46c71d
Nemo <ejongwon7@gmail.com>
2025-11-08 10:31:39 +0000

    feat: define workers' samples and ranges

--- master/src/main/scala/master/Master.scala
+++ master/src/main/scala/master/Master.scala
@@ -1,11 +1,15 @@
 package master
 
 import master.MasterService.WorkerInfo
+import scala.collection.mutable.ArrayBuffer
+import com.google.protobuf.ByteString
 
 // Master Singleton
 object Master {
   private var workersNum: Int = -1
   private var registeredWorkers = Map[String, WorkerInfo]()
+  private var samples = Map[String, Seq[ByteString]]()  // workerIp -> sampled keys
+  private var ranges = Seq[(ByteString, ByteString)]()  // (start, end) for each worker
 
   def setWorkersNum(num: Int): Unit = this.synchronized {
     workersNum = num
@@ -39,4 +43,44 @@ object Master {
   }
 
   def getRegisteredWorkers: Map[String, WorkerInfo] = this.synchronized { registeredWorkers }
+
+  def addSamples(workerIp: String, keys: Seq[ByteString]): Boolean = this.synchronized {
+    if (!registeredWorkers.contains(workerIp)) {
+      println(s"Warning: Received samples from unregistered worker: $workerIp")
+      return false
+    }
+
+    samples += (workerIp -> keys)
+
+    // If all workers have sent samples, calculate ranges
+    if (samples.size == workersNum) {
+      calculateRanges()
+    }
+
+    true
+  }
+
+  private def calculateRanges(): Unit = {
+    val comparator = ByteString.unsignedLexicographicalComparator()
+    val sortedKeys = samples.values.flatten.toArray.sortWith((a, b) => comparator.compare(a, b) < 0)
+
+    // Calculate quantiles to divide into workersNum ranges
+    val ranges = (1 to workersNum).map { i =>
+      val idx = ((i.toDouble / workersNum) * sortedKeys.length).toInt - 1
+      sortedKeys(math.max(0, idx))
+    }
+
+    // Create (start, end) pairs for each range
+    val rangePairs = ArrayBuffer[(ByteString, ByteString)]()
+    var previousKey: ByteString = ByteString.copyFrom(Array.fill[Byte](10)(0))
+    for (key <- ranges) {
+      rangePairs.append((previousKey, key))
+      previousKey = key
+    }
+    rangePairs.append((previousKey, ByteString.copyFrom(Array.fill[Byte](10)(-1))))  // Last range goes to infinity
+  }
+
+  def getRanges: Seq[(ByteString, ByteString)] = this.synchronized { ranges }
+
+  def isRangesReady: Boolean = this.synchronized { ranges.nonEmpty }
 }
