ecb9ca9df96cbff7ad07ef6e101835f8b954c1fd
Nemo <ejongwon7@gmail.com>
2025-11-08 10:31:55 +0000

    feat: define assigned ranges

--- worker/src/main/scala/worker/Worker.scala
+++ worker/src/main/scala/worker/Worker.scala
@@ -1,11 +1,14 @@
 package worker
 
+import com.google.protobuf.ByteString
+
 // Worker Singleton
 object Worker {
   private var masterIp: Option[String] = None
   private var masterPort: Option[Int] = None
   private var inputDirs: Seq[String] = Nil
   private var outputDir: Option[String] = None
+  private var assignedRange: Option[(ByteString, ByteString)] = None
 
   def setMasterAddr(ip: String, port: Int): Unit = this.synchronized {
     masterIp = Some(ip)
@@ -33,5 +36,13 @@ object Worker {
 
   def getOutputDir: Option[String] = this.synchronized {
     outputDir
+  }
+
+  def setAssignedRange(rangeStart: ByteString, rangeEnd: ByteString): Unit = this.synchronized {
+    assignedRange = Some((rangeStart, rangeEnd))
+  }
+
+  def getAssignedRange: Option[(ByteString, ByteString)] = this.synchronized {
+    assignedRange
   }
 }
