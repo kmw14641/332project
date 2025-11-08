c3ff1752662033d53f188816d15350222645add5
Nemo <ejongwon7@gmail.com>
2025-11-08 13:32:41 +0000

    Chore: writes debugging codes

--- worker/src/main/scala/worker/WorkerServiceImpl.scala
+++ worker/src/main/scala/worker/WorkerServiceImpl.scala
@@ -3,6 +3,7 @@ package worker
 import scala.concurrent.{ExecutionContext, Future}
 import worker.WorkerService.{WorkerServiceGrpc, WorkersRangeAssignment, RangeAssignment, WorkerNetworkInfo, SampledResponse, WorkerRangeAssignment}
 import io.grpc.Status
+import java.math.BigInteger
 
 class WorkerServiceImpl(implicit ec: ExecutionContext) extends WorkerServiceGrpc.WorkerService {
   override def sampled(request: WorkersRangeAssignment): Future[SampledResponse] = {
@@ -21,6 +22,13 @@ class WorkerServiceImpl(implicit ec: ExecutionContext) extends WorkerServiceGrpc
 
     // Store the assigned range in the Worker singleton
     Worker.setAssignedRange(workersRangeAssignment)
+    workersRangeAssignment.map {
+      // Print assigned ranges for debugging
+      case ((ip, port), (start, end)) =>
+        val startInt = new BigInteger(1, start.toByteArray())
+        val endInt = new BigInteger(1, end.toByteArray())
+        println(s"Assigned range to worker $ip:$port => [${startInt.toString(16)}, ${endInt.toString(16)})")
+    }
 
     Future.successful(
       SampledResponse(success = true)
