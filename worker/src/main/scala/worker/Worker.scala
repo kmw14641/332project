1b060e159a67bbc7855c53021c4c5baeec450160
kmw14641 <kmw14641@postech.ac.kr>
2025-11-09 12:32:54 +0900

    temp: assume previous state

--- worker/src/main/scala/worker/Worker.scala
+++ worker/src/main/scala/worker/Worker.scala
@@ -2,10 +2,13 @@ package worker
 
 // Worker Singleton
 object Worker {
+  val firstMergeDir: String = "/tmp/first_merge"
+
   private var masterIp: Option[String] = None
   private var masterPort: Option[Int] = None
   private var inputDirs: Seq[String] = Nil
   private var outputDir: Option[String] = None
+  private var workerInfo: List[(String, Int)] = Nil
 
   def setMasterAddr(ip: String, port: Int): Unit = this.synchronized {
     masterIp = Some(ip)
@@ -25,6 +28,14 @@ object Worker {
 
   def getInputDirs: Seq[String] = this.synchronized {
     inputDirs
+  }
+
+  def setWorkerInfo(infos: List[(String, Int)]): Unit = this.synchronized {
+    workerInfo = infos
+  }
+
+  def getWorkerInfo: List[(String, Int)] = this.synchronized {
+    workerInfo
   }
 
   def setOutputDir(dir: String): Unit = this.synchronized {
