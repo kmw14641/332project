1a30c8d845289a2f6f21748a20380c98227346c6
Nemo <ejongwon7@gmail.com>
2025-11-10 12:51:05 +0900

    feat: checks the validity of ip:port
    - check the validity of ip using InetAddress
    - check the validity of port using range 1-65535
    - check the vallidity of format "ip:port"

--- worker/src/main/scala/utils/WorkerOptionUtils.scala
+++ worker/src/main/scala/utils/WorkerOptionUtils.scala
@@ -1,6 +1,8 @@
 package utils
 
+import scala.util.Try
 import scopt.OParser
+import java.net.InetAddress
 
 case class WorkerConfig(
   masterAddr: String = "",
@@ -19,7 +21,18 @@ object WorkerOptionUtils {
         arg[String]("<master ip:port>")
           .required()
           .action((x, c) => c.copy(masterAddr = x))
-          .text("master address in format ip:port"),
+          .text("master address in format ip:port")
+          .validate(x =>
+            x.split(":") match {
+              case Array(host, portStr) =>
+                val portIsValid = Try(portStr.toInt).map(p => p > 0 && p < 65536).getOrElse(false)
+                val hostIsValid = Try(InetAddress.getByName(host)).isSuccess
+                if (hostIsValid && portIsValid) success
+                else failure("master address must be a valid ip/host and port (1-65535)")
+              case _ =>
+                failure("master address must be in format ip:port")
+            }
+          ),
         opt[String]('I', "input")
           .required()
           .valueName("<dir>")
