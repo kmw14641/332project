676ad17f79dd84c7028d3b4ead9bef010e830c64
Nemo <ejongwon7@gmail.com>
2025-11-10 12:51:59 +0900

    feat: implements custom space-separated arguments parsing algorithm
    - `worker <master ip:port> -I <input_dir_1> <input_dir_2> ... <input_dir_n> -O <output_dir>`
    - input dirs are separated by space, but it is not supported by scopt so preprocessing args as replacing spaces with commas
      - now -I can get mutiple directories

--- worker/src/main/scala/utils/WorkerOptionUtils.scala
+++ worker/src/main/scala/utils/WorkerOptionUtils.scala
@@ -1,6 +1,7 @@
 package utils
 
 import scala.util.Try
+import scala.collection.mutable.ArrayBuffer
 import scopt.OParser
 import java.net.InetAddress
 
@@ -35,9 +36,9 @@ object WorkerOptionUtils {
           ),
         opt[String]('I', "input")
           .required()
-          .valueName("<dir>")
-          .action((x, c) => c.copy(inputDirectories = x.split(" ").toSeq))
-          .text("input directory"),
+          .valueName("<dir1> <dir2> ... <dirN>")
+          .action((x, c) => c.copy(inputDirectories = x.split(",").toSeq))
+          .text("input directories (space-separated)"),
         opt[String]('O', "output")
           .required()
           .action((x, c) => c.copy(outputDirectory = x))
@@ -50,6 +51,45 @@ object WorkerOptionUtils {
     }
 
     parser
+  }
+
+  /**
+   * Preprocess command-line arguments by extracting input directories.
+   * This allows space-separated input directories after -I/--input.
+   * For example:
+   *   -I dir1 dir2 dir3
+   * is transformed to:
+   *   -I dir1,dir2,dir3
+   * 
+   * Example:
+   *  worker <master ip:port> -I dir1 dir2 dir3 -O outputDir
+   *  worker <master ip:port> -O outputDir -I dir1 dir2 dir3
+   * 
+   * @param args Original command-line arguments
+   * @return Array of processed arguments
+   */
+  private def preprocessInputDirectoriesArgs(args: Array[String]): Array[String] = {
+    val newArgs = ArrayBuffer[String]()
+    val inputDirs = ArrayBuffer[String]()
+    var i = 0
+    while (i < args.length) {
+      if (args(i) == "-I" || args(i) == "--input") {
+        i += 1
+        while (i < args.length && !args(i).startsWith("-")) {
+          inputDirs += args(i)
+          i += 1
+        }
+      } else {
+        newArgs += args(i)
+        i += 1
+      }
+    }
+
+    if (inputDirs.nonEmpty) {
+      (newArgs ++ Seq("-I", inputDirs.mkString(","))).toArray
+    } else {
+      newArgs.toArray
+    }
   }
 
   def parse(args: Array[String]): Option[(String, Seq[String], String)] = {
