b51601aee73e6e54835c1ece7b5fae13f2f21e99
kmw14641 <kmw14641@postech.ac.kr>
2025-11-12 17:32:34 +0900

    feat: implement shuffle which downloads file with limit of parallelism

--- worker/src/main/scala/client/ShuffleClient.scala
+++ worker/src/main/scala/client/ShuffleClient.scala
@@ -8,12 +8,40 @@ import shuffle.Shuffle.{ShuffleGrpc, DownloadRequest, DownloadResponse}
 import scala.async.Async.{async, await}
 
 class ShuffleClient(implicit ec: ExecutionContext) {
+    val maxRetries = 10
+    val poolSize = 50
+
     // TODO: make it global
     val stubs = Worker.getAssignedRange.get.keys.map { case (ip, port) =>
         val channel = io.grpc.ManagedChannelBuilder.forAddress(ip, port).usePlaintext().build()
         val stub = ShuffleGrpc.stub(channel)
         (ip, stub)
     }.toMap
+
+    var currentPoolSize = poolSize
+    var roundRobinIterator: Iterator[(String, String)] = Iterator.empty  // TODO: implement so that iterates receiverFileInfo in round robin
+
+	def start(receiverFileInfo: Map[String, List[String]]): Future[Unit] = async {  // TODO: make input as optional, if none, restore
+        await { expandRequestChain() }
+    }
+
+    // chain: after process file, it finds another file to process
+    // simultaneously, it immediately creates another chain by recurse so that number of chain becomes poolSize
+    private def expandRequestChain(): Future[Unit] = async {
+        tryNextRequestWithPool() match {
+            case None => ()  // end of current chain
+            case Some((worker, filename)) => {
+                val currentChain = async {
+                    await { processFile(worker, filename) }
+                    releasePool()
+                    await { expandRequestChain() } // schedule after finishing current file
+                }
+                val nextChain = expandRequestChain()  // schedule more to fill pool after creating thread
+
+                await { Future.sequence(Seq(currentChain, nextChain)) }
+            }
+        }
+    }
 
     private def processFile(worker: String, filename: String): Future[Unit] = async {
         val stub = Worker.synchronized(stubs(worker))
@@ -22,4 +50,14 @@ class ShuffleClient(implicit ec: ExecutionContext) {
         val _  = blocking { Files.write(targetPath, bytes.data.toByteArray, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING) }
     }
 
+    private def tryNextRequestWithPool() = this.synchronized {
+        if (currentPoolSize > 0 && roundRobinIterator.hasNext) {
+            currentPoolSize -= 1
+            Some(roundRobinIterator.next())
+        } else None
+    }
+
+    private def releasePool() = this.synchronized {
+        currentPoolSize += 1
+    }
 }
