5e1b6da2573d59fb1fbcdcb9c5afc4f99e6861ab
Nemo <ejongwon7@gmail.com>
2025-11-12 15:52:05 +0000

    feat: implements client who request to master rpc registerWorker
    - used by worker to register itself to master

--- common/src/main/scala/utils/SystemUtils.scala
+++ common/src/main/scala/utils/SystemUtils.scala
@@ -4,4 +4,10 @@ import java.net.InetAddress
 
 object SystemUtils {
   def getLocalIp: Option[String] = scala.util.Try(InetAddress.getLocalHost.getHostAddress).toOption
+
+  def getRamMb: Long = {
+    val runtime = Runtime.getRuntime
+    val maxMemory = runtime.maxMemory()
+    maxMemory / (1024 * 1024)
+  }
 }
---
+++ worker/src/main/scala/master/MasterClient.scala
@@ -0,0 +1,43 @@
+package master
+
+import io.grpc.{ManagedChannel, ManagedChannelBuilder}
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext}
+import master.MasterService.{MasterServiceGrpc, WorkerInfo, RegisterWorkerResponse}
+
+class MasterClient(host: String, port: Int)(implicit ec: ExecutionContext) {
+  private val channel: ManagedChannel = ManagedChannelBuilder
+    .forAddress(host, port)
+    .usePlaintext()
+    .build()
+
+  private val stub = MasterServiceGrpc.stub(channel)
+
+  def registerWorker(ip: String, port: Int, ramMb: Long): Unit = {
+    val request = WorkerInfo(
+      ip = ip,
+      port = port,
+      ramMb = ramMb
+    )
+
+    val responseFuture = stub.registerWorker(request)
+    
+    try {
+      val response = Await.result(responseFuture, 10.seconds)
+      if (!response.success) {
+        println(s"Failed to connect to master")
+        sys.exit(1)
+      }
+    } catch {
+      case e: Exception =>
+        println(s"Error registering with master: ${e.getMessage}")
+        sys.exit(1)
+    } finally {
+      shutdown()
+    }
+  }
+
+  def shutdown(): Unit = {
+    channel.shutdown()
+  }
+}
