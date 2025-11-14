679133293b8f381ab298bfebadfd32d506b4f01e
kmw14641 <kmw14641@postech.ac.kr>
2025-11-14 17:51:56 +0900

    feat: implement streaming on shuffle

--- worker/src/main/scala/client/ShuffleClient.scala
+++ worker/src/main/scala/client/ShuffleClient.scala
@@ -1,12 +1,14 @@
 package client
 
 import java.nio.file.{Files, Paths, StandardOpenOption}
-import scala.concurrent.{ExecutionContext, Future, blocking}
+import scala.concurrent.{ExecutionContext, Future, blocking, Promise}
 import worker.Worker
 import io.grpc.ManagedChannelBuilder
 import shuffle.Shuffle.{ShuffleGrpc, DownloadRequest, DownloadResponse}
 import scala.async.Async.{async, await}
 import utils.PathUtils.createDirectoryIfNotExists
+import io.grpc.stub.StreamObserver
+import java.nio.channels.FileChannel
 
 class ShuffleClient(implicit ec: ExecutionContext) {
     val maxTries = 10
@@ -20,6 +22,9 @@ class ShuffleClient(implicit ec: ExecutionContext) {
     
 	def start(receiverFileInfo: Map[String, List[String]]): Future[Unit] = async {  // TODO: make input as optional, if none, restore
         createDirectoryIfNotExists(Worker.shuffleDir)
+        receiverFileInfo.map { case (workerIp, _) =>
+            createDirectoryIfNotExists(s"${Worker.shuffleDir}/$workerIp")
+        }
         val workerFutures = receiverFileInfo.map { case (workerIp, fileList) => processFilesSequentially(workerIp, fileList) }
         await { Future.sequence(workerFutures) }
     }
@@ -28,22 +33,46 @@ class ShuffleClient(implicit ec: ExecutionContext) {
         fileList match {
             case Nil => Future.successful()
             case head :: tail => {
-                await { processFile(workerIp, head) }
-                processFilesSequentially(workerIp, tail)
+                await { processFileWithRetry(workerIp, head) }
+                await { processFilesSequentially(workerIp, tail) }
             }
         }
     }
 
     private def processFile(workerIp: String, filename: String, tries: Int = 1): Future[Unit] = {
-        async {
-            println(s"[$workerIp, $filename] SEND")
-            val stub = Worker.synchronized(stubs(workerIp))
-            val bytes: DownloadResponse = await { stub.downloadFile(DownloadRequest(filename = filename)) }
-            val targetPath = Paths.get(s"${Worker.shuffleDir}/$filename")
-            val _ = blocking { Files.write(targetPath, bytes.data.toByteArray, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING) }
-            println(s"[$workerIp, $filename] RECEIVE")
-        }.recoverWith {
+        val promise = Promise[Unit]()
+
+        val stub = Worker.synchronized(stubs(workerIp))
+        val targetPath = Paths.get(s"${Worker.shuffleDir}/$workerIp/$filename")
+        val fileChannel: FileChannel = FileChannel.open(
+            targetPath,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.WRITE,
+            StandardOpenOption.TRUNCATE_EXISTING
+        )
+        
+        println(s"[$workerIp, $filename] request")
+        stub.downloadFile(DownloadRequest(filename = filename), new StreamObserver[DownloadResponse] {
+            override def onNext(response: DownloadResponse): Unit = {
+                blocking { fileChannel.write(response.data.asReadOnlyByteBuffer()) }
+            }
+
+            override def onError(t: Throwable): Unit = promise.failure(t)
+            
+            override def onCompleted(): Unit = {
+                println(s"[$workerIp, $filename] response completed")
+                fileChannel.close()
+                promise.success(())
+            }
+        })
+
+        promise.future
+    }
+
+    private def processFileWithRetry(workerIp: String, filename: String, tries: Int = 1): Future[Unit] = {
+        processFile(workerIp, filename).recoverWith {
             case _ if tries <= maxTries => {
+                println(s"Retrying [$workerIp, $filename], attempt #$tries")
                 blocking { Thread.sleep(math.pow(2, tries).toLong) }
                 processFile(workerIp, filename, tries + 1)
             }
--- worker/src/main/scala/server/ShuffleServer.scala
+++ worker/src/main/scala/server/ShuffleServer.scala
@@ -1,16 +1,37 @@
 package server
 
-import java.nio.file.{Files, Paths}
+import java.nio.file.{Files, Paths, StandardOpenOption}
 import scala.concurrent.{ExecutionContext, Future, blocking}
 import shuffle.Shuffle.{DownloadRequest, DownloadResponse, ShuffleGrpc}
 import com.google.protobuf.ByteString
 import worker.Worker
+import java.io.InputStream
+import scala.util.Using
+import java.nio.channels.FileChannel
+import java.nio.ByteBuffer
+import io.grpc.stub.StreamObserver
 
 class ShuffleServiceImpl(implicit ec: ExecutionContext) extends ShuffleGrpc.Shuffle {
-	override def downloadFile(request: DownloadRequest): Future[DownloadResponse] = Future {
+	override def downloadFile(request: DownloadRequest, responseObserver: StreamObserver[DownloadResponse]): Unit = {
         val sourcePath = Paths.get(s"${Worker.mergeDir}/${request.filename}")
-        val bytes = blocking { Files.readAllBytes(sourcePath) }
-        DownloadResponse(data = ByteString.copyFrom(bytes))  // creates 3 copy. os heap -> jvm heap -> bytestring -> netty buffer
+        val chunkSize = 1024 * 1024 * 1
+
+        Using(FileChannel.open(sourcePath, StandardOpenOption.READ)) { channel =>
+            val buffer = ByteBuffer.allocateDirect(chunkSize)
+            var bytesRead = channel.read(buffer)
+
+            while (bytesRead != -1) {
+                buffer.flip()  // 읽기 모드로 전환 (channel이 buffer의 끝에 seek해놓은 상태임)
+                responseObserver.onNext(DownloadResponse(data = ByteString.copyFrom(buffer)))
+                buffer.clear()
+                bytesRead = channel.read(buffer)
+            }
+
+            responseObserver.onCompleted()
+        }.recover {
+            case e: Exception =>
+            responseObserver.onError(e)
+        }
     }
 }
 
