1f0e7d9889922e347a39b37bb092816cac08474e
HyeonSeo-Park11 <phs1104@postech.ac.kr>
2025-11-15 14:06:58 +0900

    feat: worker-side file list interchanging

--- worker/src/main/scala/worker/WorkerServiceImpl.scala
+++ worker/src/main/scala/worker/WorkerServiceImpl.scala
@@ -1,9 +1,10 @@
 package worker
 
 import scala.concurrent.{ExecutionContext, Future}
-import worker.WorkerService.{WorkerServiceGrpc, WorkersRangeAssignment, RangeAssignment, WorkerNetworkInfo, AssignRangesResponse, WorkerRangeAssignment}
+import worker.WorkerService.{WorkerServiceGrpc, WorkersRangeAssignment, RangeAssignment, WorkerNetworkInfo, AssignRangesResponse, WorkerRangeAssignment, FileListMessage, FileListAck, StartShuffleCommand, StartShuffleAck}
 import io.grpc.{Status, StatusException}
 import java.math.BigInteger
+import worker.sync.SynchronizationManager
 
 class WorkerServiceImpl(implicit ec: ExecutionContext) extends WorkerServiceGrpc.WorkerService {
   override def assignRanges(request: WorkersRangeAssignment): Future[AssignRangesResponse] = {
@@ -30,8 +31,33 @@ class WorkerServiceImpl(implicit ec: ExecutionContext) extends WorkerServiceGrpc
         println(s"Assigned range to worker $ip:$port => [${startInt.toString(16)}, ${endInt.toString(16)})")
     }
 
-    Future.successful(
+    val response = Future.successful(
       AssignRangesResponse(success = true)
     )
+
+    SynchronizationManager.triggerSyncPhase()
+
+    response
   }
+
+  /*
+  Receive file metadata list from a peer worker.
+  Store the incoming file plans in the Worker singleton for later processing.
+  */
+  override def deliverFileList(request: FileListMessage): Future[FileListAck] = {
+    val senderInfo = request.sender.getOrElse(
+      throw new StatusException(Status.INVALID_ARGUMENT.withDescription("Sender info is missing"))
+    )
+
+    val files = request.files.map(meta => Worker.ReceivedFileInfo(meta.fileName))
+    Worker.addIncomingFilePlan((senderInfo.ip, senderInfo.port), files)
+
+    //for debugging
+    val fileNames = files.map(_.fileName).mkString(", ")
+    println(s"[Sync][RecvList] ${senderInfo.ip}:${senderInfo.port} -> files: [$fileNames]")
+    println(s"Received ${files.size} file descriptions from ${senderInfo.ip}:${senderInfo.port}")
+
+    Future.successful(FileListAck(success = true))
+  }
+
 }
