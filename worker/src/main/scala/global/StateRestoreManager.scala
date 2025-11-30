098ba88775d31bf8fa0e1f20743ff2c3d472db8d
kmw14641 <kmw14641@postech.ac.kr>
2025-11-30 22:10:19 +0900

    feat: implement worker state serialization

---
+++ worker/src/main/scala/global/StateRestoreManager.scala
@@ -0,0 +1,44 @@
+package global
+
+import java.io._
+import global.WorkerState
+import utils.FileManager
+
+object StateRestoreManager {
+    val stateFileName: String = "worker_state"
+    def fileDir: String = s"${FileManager.getOutputDir.get}/${FileManager.stateRestoreDirName}"
+    def filePath: String = s"$fileDir/$stateFileName"
+
+    def isClean(): Boolean = {
+        !new File(filePath).exists()
+    }
+
+    def storeState(): Unit = {
+        FileManager.createDirectoryIfNotExists(fileDir)
+
+        val oos = new ObjectOutputStream(new FileOutputStream(filePath))
+        try {
+            val instance = WorkerState.getInstance
+            oos.writeObject(instance)
+        } finally {
+            oos.close()
+        }
+    }
+
+    def restoreState() = {
+        assert(!isClean())
+
+        val ois = new ObjectInputStream(new FileInputStream(filePath))
+        try {
+            val instance = ois.readObject().asInstanceOf[WorkerState]
+            WorkerState.setInstance(instance)
+        } finally {
+            ois.close()
+        }
+    }
+
+    def clear(): Unit = {
+        val file = new File(filePath)
+        if (file.exists()) file.delete()
+    }
+}
