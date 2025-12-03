edc78fdd8f37a1d264c999335103d62545e88547
kmw14641 <kmw14641@postech.ac.kr>
2025-12-04 07:46:44 +0900

    feat: introduce new worker to other workers on re-register

--- master/src/main/scala/server/RegisterServiceImpl.scala
+++ master/src/main/scala/server/RegisterServiceImpl.scala
@@ -3,11 +3,22 @@ package server
 import scala.concurrent.{ExecutionContext, Future}
 import master.MasterService.{WorkerInfo, RegisterWorkerResponse, RegisterServiceGrpc}
 import global.{MasterState, ConnectionManager}
+import scala.async.Async.{async, await}
+import worker.WorkerService.WorkerServiceGrpc
+import worker.WorkerService.WorkerNetworkInfo
 
 class RegisterServiceImpl(implicit ec: ExecutionContext) extends RegisterServiceGrpc.RegisterService {
   override def registerWorker(request: WorkerInfo): Future[RegisterWorkerResponse] = Future {
-    MasterState.registerWorker(request)
+    val faultOccured = MasterState.registerWorker(request)
     ConnectionManager.registerWorkerChannel(request.ip, request.port)
+    if (faultOccured && MasterState.isCalculateRangesStarted) async {
+      await { MasterState.assignRangesCompletedPromise.future }
+      MasterState.getRegisteredWorkers.filter(_._1 != request.ip).map { case (workerIp, _) => async {
+          val stub = WorkerServiceGrpc.stub(ConnectionManager.getWorkerChannel(workerIp))
+          await { stub.introduceNewWorker(new WorkerNetworkInfo(request.ip, request.port)) }
+        }
+      }
+    }
     RegisterWorkerResponse(success = true)
   }
 }
