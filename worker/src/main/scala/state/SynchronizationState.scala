f3bdee6cb42376800c61a2be178434273cdd7439
kmw14641 <kmw14641@postech.ac.kr>
2025-12-04 03:46:53 +0900

    feat: implement fault tolerance on reportSyncComplete, startShuffle

--- worker/src/main/scala/state/SynchronizationState.scala
+++ worker/src/main/scala/state/SynchronizationState.scala
@@ -7,9 +7,13 @@ import global.Restorable
 
 class SynchronizationState extends Serializable with Restorable {
   private var shufflePlans: Map[String, Seq[String]] = Map.empty
+  private var reportCompleted: Boolean = false
+  private var shuffleStarted: Boolean = false
   @transient private val shuffleStartPromise: Promise[Unit] = Promise[Unit]()
 
-  def restoreTransient(): Unit = {}
+  def restoreTransient(): Unit = {
+    if (shuffleStarted) shuffleStartPromise.trySuccess()
+  }
 }
 
 object SynchronizationState {
@@ -24,5 +28,21 @@ object SynchronizationState {
 
   def getShufflePlans: Map[String, Seq[String]] = WorkerState.synchronized {
     WorkerState.synchronization.shufflePlans
+  }
+
+  def completeReport() = WorkerState.synchronized {
+    WorkerState.synchronization.reportCompleted = true
+  }
+
+  def isReportCompleted = WorkerState.synchronized {
+    WorkerState.synchronization.reportCompleted
+  }
+
+  def setShuffleStarted(value: Boolean) = WorkerState.synchronized {
+    WorkerState.synchronization.shuffleStarted = value
+  }
+
+  def isShuffleStarted = WorkerState.synchronized {
+    WorkerState.synchronization.shuffleStarted
   }
 }
