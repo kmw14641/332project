d5a063fd3842833ac2335f68960351918347310c
kmw14641 <kmw14641@postech.ac.kr>
2025-12-09 22:20:27 +0900

    test

--- worker/src/main/scala/global/FileManager.scala
+++ worker/src/main/scala/global/FileManager.scala
@@ -102,7 +102,7 @@ object FileManager {
 
     Using(FileChannel.open(file, StandardOpenOption.READ)) { channel =>
       val records = Array.ofDim[Record](count)
-      val buffer = ByteBuffer.allocateDirect(channel.size.toInt)
+      val buffer = ByteBuffer.allocateDirect(BUFFER_SIZE)
       
       channel.position(offset * RECORD_SIZE)
       buffer.flip()
@@ -142,7 +142,7 @@ object FileManager {
       StandardOpenOption.WRITE,
       StandardOpenOption.TRUNCATE_EXISTING
     )) { channel =>
-      val buffer = ByteBuffer.allocateDirect(channel.size.toInt)
+      val buffer = ByteBuffer.allocateDirect(BUFFER_SIZE)
       
       var i = 0
       while (i < records.length) {
