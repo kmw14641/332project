0c16e8e4b2c695b5573cf2c06f5d5b6d80ad76f8
kmw14641 <kmw14641@postech.ac.kr>
2025-12-09 23:00:19 +0900

    test

--- worker/src/main/scala/main/FileMergeManager.scala
+++ worker/src/main/scala/main/FileMergeManager.scala
@@ -37,8 +37,10 @@ class FileMergeManager(inputSubDirName: String, outputSubDirName: String) {
           val newFilename = FileManager.getRandomFilename
           val oldFilePath = FileManager.getFilePathFromInputDir(filename)
           val newFilePath = FileManager.getFilePathFromOutputDir(newFilename)
+          logger.info(s"copy start $filenmae")
           
           FileManager.copy(oldFilePath, newFilePath)
+          logger.info(s"copy end $filenmae")
           newFilename
         }
       }}
